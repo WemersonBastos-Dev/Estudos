da273d029007a2e9ecf544fb5009321a7fc82d36
Wemerson Bastos <bastos.weeme@gmail.com>
2025-05-22 16:48:52 +0000

    Projeto Atualizado

---
+++ FUNCTION/function.js
@@ -0,0 +1,10 @@
+enviarDados()
+
+function enviarDados(){
+    let nomeDoBanco = "Banco de dados"
+    console.log ("Salvando dados em: " + nomeDoBanco)
+
+    if (3 === 3){
+        console.log("Senha validada")
+    }
+}
